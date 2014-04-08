b55876ac96b0e66e9f5081ffe4f2b22160d56607
sugilog <sugilog@gmail.com>
2014-04-08 16:41:54 +0900

    sync head and foot with body scrolling

--- fix.js
+++ fix.js
@@ -4,6 +4,7 @@
   defaultConfig = {
     width: 500,
     height: 300,
+    scrollThreshold: 3
   };
 
   frame = {
@@ -12,35 +13,29 @@
     scales: {
       columns: [],
     },
+    lastScroll: {
+      x: 0,
+      y: 0
+    },
     config: {},
     init: function( original, config ) {
+      this.config = config;
+
       this.original = jQuery( original );
       this.initScale();
 
       this.separated.head = this.original.find( "thead" );
       this.separated.body = this.original.find( "tbody" );
       this.separated.foot = this.original.find( "tfoot" );
-
-      this.config = config;
     },
     initScale: function() {
-      var columns,
-          sum = 0;
-
-      columns
-        = this.original
+      this.scales.columns =
+        this.original
           .find( "tr" ).eq( 0 )
           .find( "td, th" )
           .map( function() {
-            var width = jQuery( this ).width();
-            sum += width;
-            return width;
-          });
-
-      this.scales.columns =  columns.map( function( _, width ) {
-        console.log( width, sum );
-        return ( 100 * width / sum ) + "%";
-      }).toArray();
+            return jQuery( this ).width();
+          }).toArray();
     },
     base: function() {
       var wrapper = jQuery( "<div>" );
@@ -48,7 +43,8 @@
       wrapper.css( {
         width:     this.config.width,
         height:    this.config.height,
-        position: "relative"
+        position:  "relative",
+        overflow:  "hidden",
       });
 
       wrapper
@@ -62,13 +58,15 @@
       var wrapper = jQuery( "<div>" );
 
       wrapper
+        .addClass( "fixHead" )
         .css( {
           zIndex:   10,
           position: "absolute",
           top:   0,
           left:  0,
-          right: 0,
-          height: jQuery( this.separated.head ).height()
+          // right: 0,
+          height: jQuery( this.separated.head ).height(),
+          width:  this.original.width()
         })
         .append(
           jQuery( "<table>" ).append( this.colgroup() ).append( this.separated.head )
@@ -80,6 +78,7 @@
       var wrapper = jQuery( "<div>" );
 
       wrapper
+        .addClass( "fixBody" )
         .css( {
           zIndex:   1,
           overflow: "scroll",
@@ -87,10 +86,11 @@
           top:    jQuery( this.separated.head ).height(),
           left:   0,
           right:  0,
-          height: this.config.height - ( jQuery( this.separated.head ).height() + jQuery( this.separated.foot ).height() )
+          height: this.config.height - ( jQuery( this.separated.head ).height() + jQuery( this.separated.foot ).height() ),
+          // width:  this.original.width()
         })
         .append(
-          jQuery( "<table>" ).append( this.colgroup() ).append( this.separated.body )
+          jQuery( "<table>" ).css( { width: this.original.width() } ).append( this.colgroup() ).append( this.separated.body )
         );
 
       return wrapper;
@@ -99,13 +99,15 @@
       var wrapper = jQuery( "<div>" );
 
       wrapper
+        .addClass( "fixFoot" )
         .css( {
           zIndex:   10,
           position: "absolute",
           top:    ( this.config.height - jQuery( this.separated.foot ).height() ),
           left:   0,
-          right:  0,
-          height: jQuery( this.separated.foot ).height()
+          // right:  0,
+          height: jQuery( this.separated.foot ).height(),
+          width:  this.original.width()
         })
         .append(
           jQuery( "<table>" ).append( this.colgroup() ).append( this.separated.foot )
@@ -119,6 +121,22 @@
           return jQuery( "<col>" ).prop( { span: 1 } ).css( { width: width } );
         })
       );
+    },
+    sync: function() {
+      var self = this;
+
+      jQuery( "div.fixBody" ).on( "scroll", function( event ) {
+        // jQuery.scrollTop and scrollLeft
+        var x = jQuery( this ).scrollLeft();
+
+        if ( Math.abs( x - self.lastScroll.x ) > self.config.scrollThreshold ) {
+          self.lastScroll.x = x
+
+          jQuery( "div.fixHead, div.fixFoot" ).css( {
+            left: -1 * x
+          });
+        }
+      });
     }
   };
 
@@ -129,6 +147,7 @@
 
     frame.init( this, config );
     frame.base();
+    frame.sync();
   };
 
   jQuery.fn.fix = fix;
